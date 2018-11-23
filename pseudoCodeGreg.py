891e544fe9d416cd7feea7d537ba1dd0accb42a1
Darkyne <gregory.creupelandt@student.uclouvain.be>
2018-11-23 20:45:40 +0100

    correction pseudo code

--- pseudoCodeGreg.py
+++ pseudoCodeGreg.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, division, print_function
 
 from psychopy import core, visual, gui, data, event
 from psychopy.tools.filetools import fromFile, toFile
+from random import randint
 import time, numpy, sys, random
 import pandas as pd
 
@@ -25,90 +26,47 @@ if GUI.OK:
 else:
     sys.exit("user cancelled")
 
-## PARAMETRES
+
+
+## Help Functions
 ####################################################
-numSs = metadata[0]
-nomExp = "data_expCS"
-output = nomExp + "_Ps" + str(numSs) + "_" + time.strftime("%Y-%m-%d_%Hh%M") + ".csv"
+def createSquare(position, win, luminosity=1):
+    square = visual.Rect(win=win, name='Square',
+                width=(100, 100)[0], height=(100, 100)[1],
+                ori=0, pos=(-200, +200),
+                lineWidth=1, lineColor=[1, 1, 1], lineColorSpace='rgb',
+                fillColor=[1, 1, 1], fillColorSpace='rgb',
+                opacity=1, depth=0.0, interpolate=True)
+    square.contrast = luminosity
+    return square
 
-## ECRITURE FICHIER D'OUTPUT
 ####################################################
-fileName = numSs+ dateStr
-dataFile = open(fileName + '.csv', 'w')
-dataFile.write('baseValue	actualValue exactitude  seuil\n')
-d = pd.read_csv(fileName+".csv", sep=';')
-
-## VALUES
-####################################################
-d["initValue"]=0
-d["actualValue"]=0
-d["exactitude"]=0
-d["seuil"]=0
-
-## CARRES ET AUTRES OBJETS UTILES
-####################################################
-#creation des 20 carres (4 positions pour chacune des 5 initValue)
-
-w = visual.Window([800,600],color="black",units='pix')
-
-#nom des carres contient les luminesence en int round inferieur
-carre15TopLeft=blablabla
-carre15TopRight=blablabla
-carre15BotLeft=blablabla
-carre15BotRight=blablabla
-
-carre18TopLeft=blablabla
-carre18TopRight=blablabla
-carre18BotLeft=blablabla
-carre18BotRightt=blablabla
-
-tableauDeCarres15 = [None, carre15BopLeft, carre15BopRight, None, carre15TotLeft, carre15TotRight]
-tableauDeCarres18= [None, carre18BopLeft, carre18BopRight, None, carre18TotLeft, carre18TotRight]
-tableauDeCarres23= [None, carre23BopLeft, carre23BopRight, None, carre23TotLeft, carre23TotRight]
-tableauDeCarres29= [None, carre29BopLeft, carre29BopRight, None, carre29TotLeft, carre29TotRight]
-tableauDeCarres37= [None, carre37BopLeft, carre37BopRight, None, carre37TotLeft, carre37TotRight]
-#disposition to match 1 2 4 5 keys disposition
-
-tableauDeBasicValues = [15.0, 18.9, 23.8, 29.9, 37.7]
-tableauDeSteps = [1.585, 1.259, 1.122, 1.06, 1.0288, 1.0145, 1.007]
-globalClock = core.Clock() #measurement of the total experiment
-
-## EXPE
-####################################################
-
-# check for a keypress
-event.waitKeys()
-
-while (len(tableauDeBasicValues) >0):
-    launchExpe(tableauDeBasicValues)
-
-
-
 
 def launchExpe(tab):
 
     #preparation des carres necessaires
-    randomFirstIndex = math.random(0,tab.length)
+    randomFirstIndex = randint(0,tab.length)
     basicValue = tab[randomFirstIndex]
-    del tab[randomIndex]
+    del tab[randomFirstIndex]
     if(basicValue==15):
         carres = tableauDeCarres15
-    elif(basicValue==18):
+    elif(basicValue==18.9):
         carres = tableauDeCarres18
-    elif(basicValue==23):
-        carres = tableauDeCarres23
-    elif (basicValue == 29):
-        carres = tableauDeCarres29
-    elif (basicValue == 37):
-        carres = tableauDeCarres37
+
+    # elif(basicValue==23):
+    #     carres = tableauDeCarres23
+    # elif (basicValue == 29):
+    #     carres = tableauDeCarres29
+    # elif (basicValue == 37):
+    #     carres = tableauDeCarres37
 
     i = 0
     #if one of the counters reach 4, we stop (or actually if the tester reaches the hardest test and succeed we could also stop?)
     tableauDeFails = [0, 0, 0, 0, 0, 0, 0]
-    continuer = True
     actualDifficulty = 0
+    basicContrast = carres[0].contrast
 
-    while(continuer):
+    while(True):
 
 
         HISI = core.Clock()
@@ -128,10 +86,18 @@ def launchExpe(tab):
             numbers = [1, 2, 4, 5]
             index = random.randint(0, 3)
             squareIndex = numbers[index]
-            carres[squareIndex].setLuminosity(tableauDeSteps[actualDifficulty])  # suppose setLuminosity will multiply its luminosity by the argument
+            carres[squareIndex].contrast =  ContrastTest[actualDifficulty] #setLuminosity(tableauDeSteps[actualDifficulty])
+            # suppose setLuminosity will multiply its luminosity by the argument
             w.flip()
 
-        HISI = core.Clock()
+        carres[squareIndex].contrast=basicContrast
+        carres[1].draw()
+        carres[2].draw()
+        carres[4].draw()
+        carres[5].draw()
+        w.flip()
+
+        H = core.Clock()
         resp = event.waitKeys(keyList=['1', '2', '4', '5'])[0]
         rt = H.getTime() * 1000
 
@@ -143,7 +109,6 @@ def launchExpe(tab):
             actualDifficulty = actualDifficulty + 1  # good resp
 
             if(actualDifficulty==7): #already reached the top
-                continuer = False
                 d.seuil[i]=6
                 return #6
         else:
@@ -154,13 +119,89 @@ def launchExpe(tab):
             d.exactitude[i] = 1
             actualDifficulty=actualDifficulty-1 #diminue la difficulte
 
+            if (actualDifficulty==-1):
+                cross = visual.TextStim(w, text="YOU ARE TOO BAD", color="white", height=30, font="Courier")
+                cross.draw()
+                w.flip()
+                actualDifficulty=0
+
             for x in tableauDeFails:
                 if x>=4:
-                    continuer=False #seuil trouve
                     d.seuil[i]=actualDifficulty #difficulte précédente celle faisant 4 fails
                     return #actualDifficulty
 
         i=i+1
+
+
+## PARAMETRES
+####################################################
+numSs = metadata[0]
+nomExp = "Pokorny-Steady"
+output = nomExp + "_Ps" + str(numSs) + "_" + time.strftime("%Y-%m-%d_%Hh%M") + ".csv"
+
+## ECRITURE FICHIER D'OUTPUT
+####################################################
+fileName = output
+dataFile = open(fileName + '.csv', 'w')
+dataFile.write('baseValue	actualValue exactitude  seuil\n')
+d = pd.read_csv(fileName+".csv", sep=';')
+
+## VALUES
+####################################################
+d["initValue"]=0
+d["actualValue"]=0
+d["exactitude"]=0
+d["seuil"]=0
+
+## CARRES ET AUTRES OBJETS UTILES
+####################################################
+#creation des 20 carres (4 positions pour chacune des 5 initValue)
+
+w = visual.Window([800,600],color="grey",units='pix')
+
+#nom des carres contient les luminesence en int round inferieur
+carre15TopLeft=createSquare((-200,200),w,0.9)
+carre15TopRight=createSquare((200,200),w,0.9)
+carre15BotLeft=createSquare((-200,-200),w,0.9)
+carre15BotRight=createSquare((200,-200),w,0.9)
+
+carre18TopLeft=createSquare((-200,200),w, 0.5)
+carre18TopRight=createSquare((200,200),w,0.5)
+carre18BotLeft=createSquare((-200,-200),w,0.5)
+carre18BotRight=createSquare((200,-200),w,0.5)
+
+
+
+tableauDeCarres15 = [None, carre15BotLeft, carre15BotRight, None, carre15TopLeft, carre15TopRight]
+tableauDeCarres18= [None, carre18BotLeft, carre18BotRight, None, carre18TopLeft, carre18TopRight]
+
+
+
+# tableauDeCarres23= [None, carre23BotLeft, carre23BotRight, None, carre23TopLeft, carre23TopRight]
+# tableauDeCarres29= [None, carre29BotLeft, carre29BotRight, None, carre29TopLeft, carre29TopRight]
+# tableauDeCarres37= [None, carre37BotLeft, carre37BotRight, None, carre37TopLeft, carre37TopRight]
+
+#disposition to match 1 2 4 5 keys disposition
+
+tableauDeBasicValues = [15, 18.9] #[15.0, 18.9, 23.8, 29.9, 37.7]
+tableauDeSteps = [1.585, 1.259, 1.122, 1.06, 1.0288, 1.0145, 1.007]
+contrastTestInversed = [0.415, 0.741, 0.878, 0.94, 0.9712, 0.9855, 0.993]
+ContrastTest = [0.585, 0.259, 0.122, 0.06, 0.0288, 0.0145, 0.007]
+globalClock = core.Clock() #measurement of the total experiment
+
+## EXPE
+####################################################
+
+# check for a keypress
+event.waitKeys()
+
+while (len(tableauDeBasicValues) >0):
+    launchExpe(tableauDeBasicValues)
+
+
+
+
+
 
 
 
